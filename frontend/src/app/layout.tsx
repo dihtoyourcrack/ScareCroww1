a34ee2bd03b2b1a6cbec5a2641f3ffb4863a434d
dihtoyourcrack <jabezananiaspaul.27csa@licet.ac.in>
2026-02-08 16:47:35 +0530

    cahnge layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,8 +5,6 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { ReactNode } from "react";
 import PageTransition from "@/components/ui/PageTransition";
-import dynamic from 'next/dynamic';
-const TargetCursor = dynamic(() => import('@/components/ui/TargetCursor'), { ssr: false });
 import { WagmiConfig, createConfig, configureChains } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
@@ -51,7 +49,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               <Navigation />
               <main className="pt-24">
                 <PageTransition>{children}</PageTransition>
-                <TargetCursor />
               </main>
             </div>
           </RainbowKitProvider>
